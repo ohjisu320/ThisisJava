59dc06ea3721a5ab7acf42e7194eee0fbdc1a6de
ohjisu <ohjisu320@gmail.com>
2025-02-27 17:08:36 +0900

    ch04 end

--- thisisJava/src/ch03/sec02/ArithmeticOperatorExample.java
+++ thisisJava/src/ch03/sec02/ArithmeticOperatorExample.java
@@ -5,7 +5,7 @@ public class ArithmeticOperatorExample {
 	public static void main(String[] args) {
 		byte v1 = 10;
 		byte v2 = 4;
-		int v3 = 5;
+//		int v3 = 5;
 		long v4 = 10L;
 		
 		int result1 = v1+v2;		//모든 피연산자는 int 타입으로 자동 변환 후 연
